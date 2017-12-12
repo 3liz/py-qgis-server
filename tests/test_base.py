6a48c007aa1aa6120bff4b8ff651fe73dc96fd69
David Marteau <david@innophi.com>
2017-12-12 10:04:29 +0100

    Fix test with new url scheme

--- tests/test_base.py
+++ tests/test_base.py
@@ -14,7 +14,7 @@ def test_root_request( host ):
 def test_wms_getcaps( host ):
     """ Test 
     """
-    rv = requests.get("http://{}/services/?MAP=france_parts.qgs&SERVICE=WMS&request=GetCapabilities".format( host ))
+    rv = requests.get("http://{}/ows/?MAP=france_parts.qgs&SERVICE=WMS&request=GetCapabilities".format( host ))
     assert rv.status_code == 200
     assert rv.headers['content-type'] == 'text/xml; charset=utf-8'
 
@@ -22,7 +22,7 @@ def test_wms_getcaps( host ):
 def test_wfs_getcaps( host ):
     """ Test 
     """
-    rv = requests.get("http://{}/services/?MAP=france_parts.qgs&SERVICE=WFS&request=GetCapabilities".format( host ))
+    rv = requests.get("http://{}/ows/?MAP=france_parts.qgs&SERVICE=WFS&request=GetCapabilities".format( host ))
     assert rv.status_code == 200
     assert rv.headers['content-type'] == 'text/xml; charset=utf-8'
 
@@ -30,26 +30,26 @@ def test_wfs_getcaps( host ):
 def test_wcs_getcaps( host ):
     """ Test 
     """
-    rv = requests.get("http://{}/services/?MAP=france_parts.qgs&SERVICE=WCS&request=GetCapabilities".format( host ))
+    rv = requests.get("http://{}/ows/?MAP=france_parts.qgs&SERVICE=WCS&request=GetCapabilities".format( host ))
     assert rv.status_code == 200
     assert rv.headers['content-type'] == 'text/xml; charset=utf-8'
 
 def test_map_not_found_return_404( host ):
     """ Test that non existent map return 404
     """
-    rv = requests.get("http://{}/services/?MAP=i_do_not_exists.qgs&SERVICE=WFS&request=GetCapabilities".format( host ))
+    rv = requests.get("http://{}/ows/?MAP=i_do_not_exists.qgs&SERVICE=WFS&request=GetCapabilities".format( host ))
     assert rv.status_code == 404
 
 def test_protocol_resolution( host ):
     """ Test that custom protocol is correctly resolved
     """
-    rv = requests.get("http://{}/services/?MAP=test:france_parts.qgs&SERVICE=WFS&request=GetCapabilities".format( host ))
+    rv = requests.get("http://{}/ows/?MAP=test:france_parts.qgs&SERVICE=WFS&request=GetCapabilities".format( host ))
     assert rv.status_code == 200
 
 def test_unknown_protocol_is_404( host ):
     """ Test that custom protocol is correctly resolved
     """
-    rv = requests.get("http://{}/services/?MAP=fail:france_parts.qgs&SERVICE=WFS&request=GetCapabilities".format( host ))
+    rv = requests.get("http://{}/ows/?MAP=fail:france_parts.qgs&SERVICE=WFS&request=GetCapabilities".format( host ))
     assert rv.status_code == 404
 
 
