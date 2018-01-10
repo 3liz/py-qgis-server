90a2ccbd7bf47ec2a5907824911a055ec92eea60
David Marteau <david@innophi.com>
2018-01-10 19:20:02 +0100

    Bump revision 0.1.3a2

--- pyqgisserver/version.py
+++ pyqgisserver/version.py
@@ -1,6 +1,6 @@
 # -*- encoding=utf-8 -*-
 
-__version_info__ = (0, 1, "3a1")
+__version_info__ = (0, 1, "3a2")
 __version__ = "{0}.{1}.{2}".format(*__version_info__)
 __description__="qgis/HTTP/AMQP scalable server"
 
