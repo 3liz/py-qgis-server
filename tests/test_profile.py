5a0aac229b3b4a9ae6f64524049cef9a92e36e7d
David Marteau <david@innophi.com>
2019-01-10 15:17:18 +0100

    Support profile name as path

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -45,5 +45,16 @@ def test_ip_rejected_return_403( host ):
     assert rv.status_code == 403
 
 
+def test_profile_with_path( host ):
+    """ Test response from root path
+    """
+    url = ('/ows/p/wms/testpath?bbox=-621646.696284,5795001.359349,205707.697759,6354520.406319&crs=EPSG:3857'
+           '&dpi=96&exceptions=application/vnd.ogc.se_inimage&format=image/png&height=915'
+           '&layers=france_parts&request=GetMap'
+           '&service=WMS&styles=default&transparent=TRUE&version=1.3.0&width=1353')
+
+    rv = requests.get("http://{}{}".format( host, url ))
+    assert rv.status_code == 200
+
 
 
