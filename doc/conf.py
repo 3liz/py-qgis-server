8010ac57d42e7c287732f40ff236612abe43eb02
David Marteau <dmarteau@3liz.com>
2020-07-15 15:03:31 +0200

    Fix for RTD build

--- doc/conf.py
+++ doc/conf.py
@@ -57,3 +57,7 @@ html_theme ='sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# Fix for read the doc
+# See https://github.com/readthedocs/readthedocs.org/issues/2569
+master_doc = 'index'
+
