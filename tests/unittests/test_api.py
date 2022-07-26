8da3bc50dfa0317720999de27b701b0f8381ae0a
David Marteau <dmarteau@3liz.com>
2022-07-26 16:55:37 +0200

    Disable landing_page test

--- tests/unittests/test_api.py
+++ tests/unittests/test_api.py
@@ -14,7 +14,9 @@ class Tests(HTTPTestCase):
 
     # XXX This test takes an insane amount with QGIS >= 3.26+
     # of time on gitlab CI, this need to be investigated
-    def xxx_test_landing_page(self):
+    # See https://github.com/qgis/QGIS/pull/49476
+    @pytest.mark.skip(reason="Wait to fix project loading in landing page")
+    def test_landing_page(self):
         """ Test landing_page
         """
         rv = self.client.get( '', path="/ows/catalog/" )
